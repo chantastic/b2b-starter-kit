12b6ecbd723266f338dba0841094c56db26acff1
Michael Chan <mijoch@gmail.com>
2024-09-05 16:15:39 -0700

    remove ConvexClientProvider from Layout
    it is incompatable with `Impersonations` and 'use server' forms.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 // Import the base CSS styles for the radix-ui components.
 import "@radix-ui/themes/styles.css";
 import { AuthKitProvider } from "@workos-inc/authkit-nextjs";
-import { ConvexClientProvider } from "./convex-client-provider";
 
 import type { Metadata } from "next";
 import NextLink from "next/link";
@@ -24,46 +23,44 @@ export default function RootLayout({
     <html lang="en">
       <body style={{ padding: 0, margin: 0 }}>
         <AuthKitProvider>
-          <ConvexClientProvider>
-            <Theme
-              accentColor="iris"
-              panelBackground="solid"
-              style={{ backgroundColor: "var(--gray-1)" }}
-            >
-              <Impersonation />
+          <Theme
+            accentColor="iris"
+            panelBackground="solid"
+            style={{ backgroundColor: "var(--gray-1)" }}
+          >
+            <Impersonation />
 
-              <Container style={{ backgroundColor: "var(--gray-1)" }}>
-                <Flex direction="column" gap="5" p="5" height="100vh">
-                  <Box asChild flexGrow="1">
-                    <Card size="4">
-                      <Flex direction="column" height="100%">
-                        <Flex asChild justify="between">
-                          <header>
-                            <Flex gap="4">
-                              <Button asChild variant="soft">
-                                <NextLink href="/">Home</NextLink>
-                              </Button>
+            <Container style={{ backgroundColor: "var(--gray-1)" }}>
+              <Flex direction="column" gap="5" p="5" height="100vh">
+                <Box asChild flexGrow="1">
+                  <Card size="4">
+                    <Flex direction="column" height="100%">
+                      <Flex asChild justify="between">
+                        <header>
+                          <Flex gap="4">
+                            <Button asChild variant="soft">
+                              <NextLink href="/">Home</NextLink>
+                            </Button>
 
-                              <Button asChild variant="soft">
-                                <NextLink href="/account">Account</NextLink>
-                              </Button>
-                            </Flex>
+                            <Button asChild variant="soft">
+                              <NextLink href="/account">Account</NextLink>
+                            </Button>
+                          </Flex>
 
-                            <SignInButton />
-                          </header>
-                        </Flex>
-
-                        <Flex flexGrow="1" align="center" justify="center">
-                          <main>{children}</main>
-                        </Flex>
+                          <SignInButton />
+                        </header>
                       </Flex>
-                    </Card>
-                  </Box>
-                  <Footer />
-                </Flex>
-              </Container>
-            </Theme>
-          </ConvexClientProvider>
+
+                      <Flex flexGrow="1" align="center" justify="center">
+                        <main>{children}</main>
+                      </Flex>
+                    </Flex>
+                  </Card>
+                </Box>
+                <Footer />
+              </Flex>
+            </Container>
+          </Theme>
         </AuthKitProvider>
       </body>
     </html>
